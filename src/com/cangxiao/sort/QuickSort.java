a1bbe702a3938ca6ea75d87c031416451d1cb30a
cangxiao <zhandchn@163.com>
2021-07-03 16:18:49 +0800

    快速排序

--- src/com/cangxiao/sort/QuickSort.java
+++ src/com/cangxiao/sort/QuickSort.java
@@ -4,54 +4,56 @@ import java.util.Arrays;
 
 /**
  * @Author cangxiao
- * @Date 2021/3/31
+ * @Date 2021/7/1
  * @Desc 快速排序
  */
 public class QuickSort {
 
     public static void main(String[] args) {
-        int[] array = {13,15,24,99,14,11,1,2,3};
+//        int[] array = {13,15,24,99,14,11,1,2,3};
+//        int[] array = {6,11,3,9,8,6};
+        int[] array = {1,2,3,4,5,6};
         QuickSort quickSort = new QuickSort();
-        quickSort.quickSort(array,0,array.length-1);
+        quickSort.sort(array,0,array.length-1);
         System.out.println(Arrays.toString(array));
     }
 
-    public void quickSort(int[] array,int left,int right){
-        if(left<right){
-            int middle = sort(array, left, right);
-            //每次只需quickSort将原数组一份为2；
-            quickSort(array, left, middle-1);
-            quickSort(array, middle+1, right);
-        }
+    public void sort(int[] array, int p, int r){
+        if(p>=r) return;
+        int q = partition(array,p,r);
+        sort(array,p,q-1);
+        sort(array,q+1,r);
     }
 
-    public int sort(int[] array,int left,int right){
-
-//        int middle = (left+right)/2;
-        //选择一个基准值，大于基准值的放右边，小于基准值的放左边
+    /**
+     * i 指向大于分区点的值，j 用于扫描整个数组，
+     * 如果 i 指向的值小于分区值，则 i+1；
+     * 如果 i 指向的值大于分区值，i 不变，j 继续扫描数组，
+     * 直到 j指向的值小于分区值，交换这 i 和 j 索引值的位置，然后 i+1(经过交换后，i 索引的值小于分区值吗，所以 i 需要加1)
+     * 如果 i 和 j 相等说明指向同一个值，则不需要交换，
+     * @return
+     */
+    public int partition(int[] array, int p, int q){
+        int i = p;
+        int pivot = array[q];
+        for (int j = i; j < q; j++) {
+            //i 指向大于pivot的值，如果当前值小于pivot ,则交换位置
+            if(array[j]<pivot) {
+                if(i!=j){
+                    swap(array,i , j);
+                }
+                i++;
+            }
+            System.out.println(Arrays.toString(array));
+        }
+        //最后交换i和pivot的位置；将pivot的值放到数组中间
+        swap(array,i,q);
+        System.out.println(" i = " + i);
+        return i;
+    }
+    public void swap(int[] array, int left, int right){
         int temp = array[left];
-        while (left<right){
-            //从右往左，右边的值大于基准值，不需要交换位置，比较下一个值
-            while (temp<array[right]){
-                right--;
-            }
-            //当右边的值小于基准值时，交换位置，将右边小的值放到左边
-            swap(array,left,right);
-            //从左往右，基准值大于左边的值，不需要交换位置，比较下一个值
-            while (temp>array[left]){
-                left++;
-            }
-            //当左边的值大于基准值时，交换位置，将左边大的值放到右边
-            swap(array,left,right);
-        }
-        //排序完，基准值都会排到数组的中间位置；即 left==right时
-        int middle = left;
-        return middle;
-    }
-
-    private void swap(int[] array,int left, int right) {
-        int t = array[left];
         array[left] = array[right];
-        array[right] = t;
+        array[right] = temp;
     }
 }
---
+++ src/com/cangxiao/sort/QuickSort3.java
@@ -0,0 +1,69 @@
+package com.cangxiao.sort;
+
+import java.util.Arrays;
+
+/**
+ * @Author cangxiao
+ * @Date 2021/7/2
+ * @Desc
+ */
+public class QuickSort3 {
+
+
+    public static void main(String[] args) {
+//        int[] array = {13, 15, 24, 99, 14, 11, 1, 2, 3};
+        int[] array = {6, 11, 9, 3, 8, 6};
+//        int[] array = {4, 3, 2, 1, 5, 4};
+        QuickSort3 quickSort= new QuickSort3();
+        quickSort.sort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public void sort(int[] array, int left, int right) {
+        if (left < right) {
+            //获取基准值索引进行分区
+            int index = partition(array, left, right);
+            //每次只需quickSort将原数组一份为2；
+            sort(array, left, index - 1);
+            sort(array, index + 1, right);
+        }
+    }
+
+    public int partition(int[] array, int left, int right) {
+
+        //选择一个基准值，大于基准值的放右边，小于基准值的放左边
+        int temp = array[left];
+        while (left < right) {
+            //从右往左，右边的值大于基准值，不需要交换位置，比较下一个值
+            while (left < right && temp <= array[right]) {
+                right--;
+            }
+            //当右边的值小于基准值时，交换位置，将右边小的值放到左边
+            if(left!=right){
+                System.out.println(array[left]);
+                swap(array, left, right);
+            }
+            //从左往右，基准值大于左边的值，不需要交换位置，比较下一个值
+            while (left < right && temp > array[left]) {
+                left++;
+            }
+            //当左边的值大于基准值时，交换位置，将左边大的值放到右边
+            if(left!=right){
+                System.out.println(array[right]);
+                swap(array, left, right);
+            }
+        }
+        //排序完，基准值都会排到数组的中间位置；即 left==right时
+        int index = left;
+        return index;
+    }
+
+    private void swap(int[] array, int left, int right) {
+
+        int t = array[left];
+        array[left] = array[right];
+        array[right] = t;
+    }
+
+
+}
